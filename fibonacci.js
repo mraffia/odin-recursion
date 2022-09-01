d14c9a723f3c3d68cd8ea81d12c208b20678f17a
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-01 18:28:20 +0700

    Implement function fibsRec

--- fibonacci.js
+++ fibonacci.js
@@ -1,24 +1,40 @@
 function fibs(num) {
-    let result = [0, 1];
-
-    if (num <= 0) {
-        return [];
-    } else if (num === 1) {
+    if (num <= 1) {
         return [0];
     } else if (num === 2) {
         return [0, 1];
     } else {
+        let result = [0, 1];
+
         for (let i = 1; i < num - 1; i++) {
             let before = result[i];
             let beforeTwo = result[i - 1];
             result.push(before + beforeTwo);
         }
+
         return result;
     }
 }
 
-console.log(fibs(0));
 console.log(fibs(1));
 console.log(fibs(2));
 console.log(fibs(3));
 console.log(fibs(8));
+
+function fibsRec(num) {
+    if (num <= 1) {
+        return [0];
+    } else if (num === 2) {
+        return [0, 1];
+    } else {
+        let result = fibsRec(num - 1);
+        result.push(fibsRec(num - 1).at(-1) + fibsRec(num - 2).at(-1));
+
+        return result;
+    }
+}
+
+console.log(fibsRec(1));
+console.log(fibsRec(2));
+console.log(fibsRec(3));
+console.log(fibsRec(8));
